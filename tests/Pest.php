72072bd938a6bb94031ef68c2eba18b163e0bb89
Erika Heidi <erika@erikaheidi.com>
2024-09-25 12:25:21 +0200

    Fixing tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
@@ -24,9 +26,7 @@
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+expect()->extend('toBeOne', fn () => $this->toBe(1));
 
 /*
 |--------------------------------------------------------------------------
@@ -40,41 +40,22 @@ expect()->extend('toBeOne', function () {
 */
 
 use Librarian\Provider\ContentServiceProvider;
-use Librarian\Provider\LibrarianServiceProvider;
-use Librarian\Provider\RouterServiceProvider;
-use Librarian\Provider\TwigServiceProvider;
 use Minicli\App;
 use Minicli\Command\CommandCall;
 
-function getCommandsPath(): string
-{
-    return __DIR__ . '/../app/Command';
-}
-
 function getApp(): App
 {
-    return new App(load_config(__DIR__ . '/../config'));
-}
+    $app = new App([],
+        'Type "./librarian help" for help with available commands.',
+        __DIR__.'/../'
+    );
 
-function getWebApp(): App
-{
-    $app = getApp();
-    $app->addService('content', new ContentServiceProvider());
-    $app->addService('twig', new TwigServiceProvider());
-    $app->addService('librarian', new LibrarianServiceProvider());
-    $app->addService('router', new RouterServiceProvider());
+    $app->addService('content', new ContentServiceProvider);
 
     return $app;
 }
 
-function getConfigValue(string $key): mixed
-{
-    $config = load_config();
-
-    return $config[$key] ?? null;
-}
-
-function getCommandCall(array $parameters = null): CommandCall
+function getCommandCall(?array $parameters = null): CommandCall
 {
     return new CommandCall(array_merge(['minicli'], $parameters));
 }
